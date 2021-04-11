4d9b6325b870c550f65984f73572242513c4fdb5
asvaaron <asvaaron@gmail.com>
2021-04-11 17:38:53 -0600

    Validate arguments
    Validate all type for of arguments

--- main.c
+++ main.c
@@ -4,5 +4,15 @@
 
 int main(int argc, char **argv) {
 
+/* iterate over all arguments */
+   for (int i = 1; i < argc; i++) {
+       if (strcmp("-V", argv[i]) == 0) {
+          printf("Verbose \n");
+       }
+       if (strcmp("-v", argv[i]) == 0) {
+          printf("verbose \n ");
+       }
+       printf("argument %s\n", argv[i]);
+   }
     return 0; 
 }
