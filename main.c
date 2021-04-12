22386be81b6786adc149116b80d5a048c19f22e9
asvaaron <asvaaron@gmail.com>
2021-04-11 19:15:30 -0600

    Remove not used includes
    Move code to an indepent function
    Test function

--- main.c
+++ main.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
 #include <ctype.h>
-#include <pthread.h>
+#include "include/validator.h"
 
 int main(int argc, char **argv) {
-
-/* iterate over all arguments */
-   for (int i = 1; i < argc; i++) {
-       if (strcmp("-V", argv[i]) == 0) {
-          printf("Verbose \n");
-       }
-       if (strcmp("-v", argv[i]) == 0) {
-          printf("verbose \n ");
-       }
-       printf("argument %s\n", argv[i]);
-   }
+    char * type = argument_type(argc, argv);
+    printf("Type %s", type);
     return 0; 
 }
