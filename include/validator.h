0207f064ca70070ca4821eea8f0fa8ca5d0c7863
asvaaron <asvaaron@gmail.com>
2021-04-12 14:46:56 -0600

    Add new attributes to the struct
    Add wait flag
    Add program commands size

--- include/validator.h
+++ include/validator.h
@@ -1,8 +1,12 @@
+#include <string.h>
 
 typedef struct ArgumentsFields {
    char * flag;
-
+   int wait;
+   char ** program_command;
+   int program_command_size;
 } ArgumentsFields;
 
 ArgumentsFields* argument_type(int, char **);
 
+void concat_strings(char *, char *);
--- src/validator.c
+++ src/validator.c
@@ -2,15 +2,31 @@
 
 
 ArgumentsFields* argument_type(int argc, char ** argv){
-   ArgumentsFields * arguments = malloc(sizeof(ArgumentsFields));; 
+   ArgumentsFields * arguments = malloc(sizeof(ArgumentsFields));
+   char * program_command[argc-1]; 
+   int j = 0;
+   int wait = 0;
    for (int i = 1; i < argc; i++) {
-      if (strcmp("-V", argv[i]) == 0) {
-         arguments -> flag = argv[i];
-      }
       if (strcmp("-v", argv[i]) == 0) {
-         arguments -> flag = argv[i];
+         arguments->flag = argv[i];
       }
-    }
+   
+      if (strcmp("-V", argv[i]) == 0) {
+         arguments->flag = argv[i];
+         if(wait ==0){
+            wait = 1;
+         }
+      }
+      if(strcmp("-v", argv[i]) != 0 && strcmp("-V", argv[i])!=0){
+         program_command[j++]=argv[i];
+      }
+   }
+   arguments ->program_command = program_command;
+   arguments -> wait = wait;
+   arguments ->program_command_size = j;
+   return arguments;
+}
 
-    return arguments;
+void concat_strings(char * a, char * b){
+   return 0;
 }
