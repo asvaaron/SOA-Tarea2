312274b122c58755f8a7135f6dd0e818e3b005a4
asvaaron <asvaaron@gmail.com>
2021-04-25 16:33:42 -0600

    Remove printf
    Remove printf from main

--- main.c
+++ main.c
@@ -5,17 +5,13 @@
 
 int main(int argc, char **argv) {
     struct_arguments_fields * arguments = argument_type(argc, argv);
-    printf("Wait %d\n", arguments -> wait);
-    printf("program %s\n", arguments -> program_command);
-    printf("arguments size %d\n", arguments -> program_command_args_size);
     int syscall[350] = {0};
     trace_app(arguments->program_command, arguments->args, syscall, arguments -> wait);
 //    for (int i = 0; i < arguments->program_command_args_size; ++i) {
 //        fprintf(stdout,"Param: %s\n", arguments->args[i]);
 //    }
-//    for (int i = 0; i < 350; ++i) {
-//        printf("System call: %d, %d\n", i, syscall[i]);
-//    }
+
+    print_system_calls_table_info(syscall, 350);
 
 
 
